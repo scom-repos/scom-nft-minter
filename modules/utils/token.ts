adfa8124b99d64c47be0bfbaafd50c9095ce151b
celina-ijs <celina.dev@instajs.com>
2023-02-13 19:01:01 +0700

    Add try catch to get data

--- modules/utils/token.ts
+++ modules/utils/token.ts
@@ -2,8 +2,12 @@ import { BigNumber, Erc20, Wallet, IWallet, ISendTxEventsOptions } from "@ijstec
 import { ITokenObject } from "@modules/interface";
 
 export const getERC20Amount = async (wallet: IWallet, tokenAddress: string, decimals: number) => {
-  let erc20 = new Erc20(wallet, tokenAddress, decimals);
-  return await erc20.balance;
+  try {
+    let erc20 = new Erc20(wallet, tokenAddress, decimals);
+    return await erc20.balance;
+  } catch {
+    return new BigNumber(0);
+  }
 }
 
 export const getTokenBalance = async (token: ITokenObject) => {
